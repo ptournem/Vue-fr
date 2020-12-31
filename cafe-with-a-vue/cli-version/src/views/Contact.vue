13df11831a09ea489b77dea5531c669b0e0a43c1
Paul Tournemaine <p.tournemaine@a2si.net>
2020-12-31 02:09:21 +0100

    CHANGE - Exercice P2C3 Done

---
+++ cafe-with-a-vue/cli-version/src/views/Contact.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<h2>Contactez nous</h2>
+		<p>Adresse : {{ address }}</p>
+		<p>Téléphone : {{ phone }}</p>
+		<p>Email : {{ email }}</p>
+		<p>Horaires :</p>
+		<ul>
+			<li>L-V: 06:00 à 16:00</li>
+			<li>Samedi: 07:00 à 14:00</li>
+			<li>Dimanche: 07:00 à 12:00</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Home",
+	data() {
+		return {
+			address: "18 avenue du Beurre, Paris, France",
+			email: "hello@cafewithavue.bakery",
+			phone: "01 88 88 88 88",
+			restaurantName: "La belle vue",
+			
+		}
+	}
+}
+
+</script>
